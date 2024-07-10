d962648edb765a848a237cc267399b91b9d1f2da
Erik Bauer <erbauer@ethz.ch>
2024-07-10 17:38:38 +0200

    arctic dataset conversion works

--- arctic_dataset/arctic_dataset_dataset_builder.py
+++ arctic_dataset/arctic_dataset_dataset_builder.py
@@ -4,6 +4,7 @@ import glob
 import numpy as np
 import tensorflow as tf
 import tqdm
+import time
 import tensorflow_datasets as tfds
 import tensorflow_hub as hub
 from dexformer.dataset import h5_tools
@@ -17,6 +18,7 @@ from human_data_preprocessing import (
     parse_sequences,
     extract_sequence_data,
     build_obs_action_list,
+    load_images,
 )
 
 
@@ -44,15 +46,15 @@ class ArcticDataset(tfds.core.GeneratorBasedBuilder):
                             "observation": tfds.features.FeaturesDict(
                                 {
                                     "image": tfds.features.Image(
-                                        shape=(540, 960, 3),
+                                        shape=(256, 256, 3),
                                         dtype=np.uint8,
                                         encoding_format="png",
                                         doc="RGB image of the scene.",
                                     ),
                                     "state": tfds.features.Tensor(
-                                        shape=(17,),
+                                        shape=(102,),
                                         dtype=np.float32,
-                                        doc="Two human hands, absolute poses (using XYZ Euler) in the workspace and 45-dimensional MANO parameters. [pose_r, pose_l, mano_r, mano_l]",
+                                        doc="At the moment, just zeros.",
                                     ),
                                 }
                             ),
@@ -79,15 +81,15 @@ class ArcticDataset(tfds.core.GeneratorBasedBuilder):
                                 dtype=np.bool_,
                                 doc="True on last step of the episode if it is a terminal step, True for demos.",
                             ),
-                            # "language_instruction": tfds.features.Text(
-                            #     doc="Language Instruction."
-                            # ),
-                            # "language_embedding": tfds.features.Tensor(
-                            #     shape=(512,),
-                            #     dtype=np.float32,
-                            #     doc="Kona language embedding. "
-                            #     "See https://tfhub.dev/google/universal-sentence-encoder-large/5",
-                            # ),
+                            "language_instruction": tfds.features.Text(
+                                doc="Language Instruction."
+                            ),
+                            "language_embedding": tfds.features.Tensor(
+                                shape=(512,),
+                                dtype=np.float32,
+                                doc="Kona language embedding. "
+                                "See https://tfhub.dev/google/universal-sentence-encoder-large/5",
+                            ),
                         }
                     ),
                     "episode_metadata": tfds.features.FeaturesDict(
@@ -118,50 +120,60 @@ class ArcticDataset(tfds.core.GeneratorBasedBuilder):
 
         """
 
-        def _parse_example(seq_data, image_generator):
+        def _parse_example(seq_path, seq_data, image_generator):
             # load raw data --> this should change for your dataset
             # this is a list of dicts in our case
             # data = np.load(episode_path, allow_pickle=True)
-            view_ids = [str(i) for i in range(9)]
+            # view_ids = [str(i) for i in range(1)]
+            view_ids = [
+                "0",
+            ]
 
+            start_time = time.time()
             data_dict = extract_sequence_data(seq_data, image_generator, view_ids)
+            print(f"Extracted data in {time.time() - start_time:.2f}s")
 
-            episode_data = build_obs_action_list(data_dict)
+            episode_data = build_obs_action_list(
+                dataset_name="arctic", human_data_dict=data_dict
+            )
 
             #
             # # TODO: get better language descriptions
-            # language_desc = task_name
+            language_desc = ""
             #
             # assemble episode --> here we're assuming demos so we set reward to 1 at the end
             episode = []
             for i, step in enumerate(episode_data):
                 # compute Kona language embedding
-                # language_embedding = self._embed(
-                #     [step['language_instruction']])[0].numpy()
-                # language_embedding = self._embed([language_desc])[0].numpy()
+                # language_embedding = self._embed([step["language_instruction"]])[
+                # 0
+                # ].numpy()
+                language_embedding = self._embed([language_desc])[0].numpy()
                 episode.append(
                     {
                         **step,
                         "discount": 1.0,
-                        "reward": float(i == (len(sync_dataframe) - 1)),
+                        "reward": float(i == (len(episode_data) - 1)),
                         "is_first": i == 0,
-                        "is_last": i == (len(sync_dataframe) - 1),
-                        "is_terminal": i == (len(sync_dataframe) - 1),
-                        # "language_instruction": language_desc,
-                        # "language_embedding": language_embedding,
+                        "is_last": i == (len(episode_data) - 1),
+                        "is_terminal": i == (len(episode_data) - 1),
+                        "language_instruction": language_desc,
+                        "language_embedding": language_embedding,
                     }
                 )
 
             # create output data sample
-            sample = {"steps": episode, "episode_metadata": {"file_path": episode_path}}
+            sample = {"steps": episode, "episode_metadata": {"file_path": seq_path}}
 
             # if you want to skip an example for whatever reason, simply return None
-            return episode_path, sample
+            return seq_path, sample
 
         # for smallish datasets, use single-thread parsing
-        for seq_data, im_data in parse_sequences(seq_dir, im_dir):
-            image_generator = load_images(im_data)
-            yield _parse_example(seq_data, image_generator)
+        seq_idx = 0
+        for seq_path, seq_data, image_generator in parse_sequences(seq_dir, im_dir):
+            # image_generator = load_images(im_data)
+            print(f"Processing sequence {seq_idx}")
+            yield _parse_example(seq_path, seq_data, image_generator)
 
         # for large datasets use beam to parallelize data parsing (this will have initialization overhead)
         # beam = tfds.core.lazy_imports.apache_beam
